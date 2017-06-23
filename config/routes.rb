8e1a6b85ec5f081ef084f8cbf9fb81a586b794ba
Jason Roodman <jason@Jasons-MacBook-Pro.local>
2017-06-23 17:11:08 -0400

    comments functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,20 @@ Rails.application.routes.draw do
   post '/login', to: 'session#create'
   delete '/logout', to: 'session#destroy'
 
+  post '/links/:id/comment', to: 'comments#create_link_comment'
+  post '/comments/:id/comment', to: 'comments#create_comment_comment'
+
   resources :users, only: [:new, :create, :edit, :update]
-  resources :links, only: [:new, :create]
+  resources :links, only: [:new, :create] do
+    member do
+      post :create_link_comment, controller: :comments, as: :create_comment_for
+    end
+  end
+  resources :comments, only: [:index, :new, :destroy] do
+    member do
+      post :create_child, controller: :comments
+    end
+  end
   resources :votes, only: [:create, :destroy]
 
 end
