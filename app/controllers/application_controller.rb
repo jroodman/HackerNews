235820518776043a6dc62b86a98b707763a12fda
Jason Roodman <jason@Jasons-MacBook-Pro.local>
2017-06-26 10:36:30 -0400

    modified current_user lookup for efficiency

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :set_current_user
+
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    @current_user
   end
   helper_method(:current_user)
 
